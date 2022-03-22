c9111ce6552df7c80b99f5ac368ec414fd72b1ff
hugosenari <hugosenari@gmail.com>
2022-03-22 00:33:57 -0300

    chore: re-index ../nimpkgs/l/libgit2

--- master/flake.nix
+++ master/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-libgit2-master.flake = false;
   inputs.src-libgit2-master.owner = "barcharcraz";
-  inputs.src-libgit2-master.ref   = "refs/heads/master";
+  inputs.src-libgit2-master.ref   = "master";
   inputs.src-libgit2-master.repo  = "libgit2-nim";
   inputs.src-libgit2-master.type  = "github";
   
